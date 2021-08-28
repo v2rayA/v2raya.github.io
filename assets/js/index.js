aaf4ab39b84343287d9aa71b7c875f146212dd96
cubercsl <2014cais01@gmail.com>
2021-08-28 22:14:20 +0800

    feat(flexsearch): search i18n index (#20)
    * add a section about how to set environment and arguments (#17)
    
    * add a section about how to set environment and arguments
    
    * lint
    
    * complete use other core (#18)
    
    * complete use other core
    
    * Update use-other-core.md
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    * complete quick start (#19)
    
    * complete quick start
    
    * directory structure
    
    * forgot to git add
    
    * Apply suggestions from code review
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    * feat(flexsearch): search i18n index
    
    * feat(shortcode): use notice shortcode (warning, info, note, tip) (#21)
    
    * add a section about how to set environment and arguments (#17)
    
    * add a section about how to set environment and arguments
    
    * lint
    
    * complete use other core (#18)
    
    * complete use other core
    
    * Update use-other-core.md
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    * complete quick start (#19)
    
    * complete quick start
    
    * directory structure
    
    * forgot to git add
    
    * Apply suggestions from code review
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    Co-authored-by: cubercsl <2014cais01@gmail.com>
    
    * feat(shortcode): use notice shortcode (warning, info, note, tip)
    
    * fix
    
    Co-authored-by: mzz <m@mzz.pub>
    
    Co-authored-by: mzz <m@mzz.pub>

--- assets/js/index.js
+++ assets/js/index.js
@@ -76,23 +76,6 @@ Source:
     }
   });
 
-
-  // Not yet supported: https://github.com/nextapps-de/flexsearch#complex-documents
-
-  /*
-  var docs = [
-    {{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
-      {
-        id: {{ $index }},
-        href: "{{ .Permalink }}",
-        title: {{ .Title | jsonify }},
-        description: {{ .Params.description | jsonify }},
-        content: {{ .Content | jsonify }}
-      },
-    {{ end -}}
-  ];
-  */
-
   // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
 
   {{ $list := (where .Site.Pages "Section" "docs") -}}
